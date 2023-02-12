fa902a0ddb3807922a64b4b4d403d72981125e70
Niyue1998 <1508748179@qq.com>
2023-02-12 17:35:43 +0800

    添加文件

---
+++ HelloGit/src/main/java/com/itcast/git/HelloGit.java
@@ -0,0 +1,11 @@
+package com.itcast.git;
+
+/**
+ * @author niyue
+ * @create 2023-02-12 17:25
+ */
+public class HelloGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git!");
+    }
+}
